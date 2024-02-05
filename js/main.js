2d097224b7ab5bf37066512dbcece14a4c8adb16
EdwingDuvanHernandezHerrera <124322869+EdwingDuvanHernandezHerrera@users.noreply.github.com>
2024-02-04 22:19:40 -0500

    feat 🍻 Se crea el modal carrito de compras
    Se crea la vista modal de carrito de compras con html, css y JavaScript
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -4,7 +4,19 @@ const menuLinks = document.querySelectorAll('.navButtons a[href^="#"');
 
 menuLinks.forEach(menuLink =>{
     menuLink.addEventListener('click', function(){
-        // menu.style.opacity = 0;
         checkBox.checked = false;
     })
+})
+
+const btnAbrirModal = document.querySelector("#btnAbrirModal");
+const btnCerrarModal =  document.querySelector('#btnCerrarModal');
+const modal = document.querySelector('#carritoModal');
+
+
+btnAbrirModal.addEventListener("click", ()=>{
+    modal.showModal();
+})
+
+btnCerrarModal.addEventListener("click", ()=>{
+    modal.close();
 })
